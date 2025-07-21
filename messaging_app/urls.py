699eb7486e94784feefd7402c0a2e55bd428d6e2
Shema Cedric Landry <88650841+Shema21@users.noreply.github.com>
2025-07-21 08:36:30 +0100

    Update urls.py

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -4,6 +4,19 @@ from chats import auth
 from rest_framework.routers import DefaultRouter
 from chats.views import ConversationViewSet, MessageViewSet
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Your API",
+      default_version='v1',
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
@@ -16,4 +29,10 @@ urlpatterns = [
     # JWT endpoints
     path('api/token/', auth.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', auth.TokenRefreshView.as_view(), name='token_refresh'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
+
+
+
+
